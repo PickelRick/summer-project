8517c271a04adedbd3be9667abe7eac3df0c0f92
wangh <wangh@DESKTOP-SBS39OE>
2018-06-19 06:53:52 +0100

    first week

---
+++ JVM/src/JVM/ByteCodeReader.java
@@ -0,0 +1,17 @@
+package JVM;
+
+import java.util.ArrayList;
+
+public class ByteCodeReader {
+	private ArrayList<String[]> result;
+	public ArrayList<String[]> transfer(String byteCode){
+		result = new ArrayList<String[]>();
+		String[] lines = byteCode.split("\n");
+		
+		int line = lines.length;
+		for(int i = 0; i < line ;i++) {
+			result.add(lines[i].split(": |_|#|Method "));
+		}
+		return result;
+	}
+}
--- JVM/src/JVM/Frame.java
+++ JVM/src/JVM/Frame.java
@@ -1,5 +1,6 @@
 package JVM;
 import java.util.*;
+import java.util.regex.*;
 
 
 public class Frame {
@@ -30,7 +31,7 @@ public class Frame {
 			return this.operandStack.pop();
 		}else {
 			this.stackCounter = 0;
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("cannot pop from empty stack");
 		}
 	}
 	
@@ -42,7 +43,7 @@ public class Frame {
 		if(localVariables.containsKey(slot)) {
 			return localVariables.get(slot);
 		}else {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("do not have this slot");
 		}
 	}
 	
@@ -61,4 +62,44 @@ public class Frame {
 	public String getCallerFrame() {
 		return this.callerFrame;
 	}
+	
+	public double returnValue() {
+		return this.pop();
+	}
+	
+	public void excute(String[] byteCodeLine) {
+		Pattern pc = Pattern.compile("[0-9]*");
+		Pattern op = Pattern.compile("(i|d)[a-z]*");
+		Matcher pm = pc.matcher(byteCodeLine[1]);
+		Matcher om = op.matcher(byteCodeLine[2]);
+		if(pm.matches()) {
+			this.programCounter = Integer.parseInt(byteCodeLine[1]);
+			//change the pc
+			if(om.matches()) {
+				String opcode = byteCodeLine[2].substring(1);
+				if(opcode.equals("load")) {
+					int slot = Integer.parseInt(byteCodeLine[3]);
+					this.push(localVariables.get(slot));
+				}else if(opcode.equals("sub")) {
+					this.push(-1*(this.pop()-this.pop()));
+				}else if(opcode.equals("mul")) {
+					this.push(this.pop()*this.pop());
+				}else if(opcode.equals("const")) {
+					double operand = Double.parseDouble(byteCodeLine[3]);
+					this.push(operand);
+				}else if(opcode.equals("return")) {
+					this.returnValue();
+				}else {
+					throw new IllegalArgumentException("Opps, I do not know this byte code");
+				}
+			}else {
+				throw new IllegalArgumentException("Illegal format(opcode)");
+			}
+			
+		}else {
+			throw new IllegalArgumentException("Illegal format(pc)");
+		}
+	}
+	
+	
 }
